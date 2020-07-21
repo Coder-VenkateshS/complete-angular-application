8fb2904c8e89bdc07eac6bd99f04289a469c784b
Teacher-Venkatesh <12venkat21@gmail.com>
2020-07-21 13:10:40 +0530

    complete angular stub

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AchievementsComponent } from './components/achievements/achievements.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',redirectTo:'/home',pathMatch:'full'
+  },
+ 
+  {
+    path:'home',component:HomeComponent,
+  },
+  {
+    path:'about',component:AboutComponent
+  },
+  {
+    path:'contacts', component:ContactComponent
+  },
+  {
+    path:'achievements',component:AchievementsComponent
+  },
+  {
+    path:'gallery',component:GalleryComponent
+  },
+  {
+    path:'feedback',component:FeedbackComponent
+  },
+  {
+    path:'login',component:LoginComponent
+  },
+  {
+    path:'signup',component:SignupComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,62 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { MainComponent } from './components/main/main.component';
+import { FetchService } from './services/fetch.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AchievementsComponent } from './components/achievements/achievements.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { HomeComponent } from './components/home/home.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    MainComponent,
+    AboutComponent,
+    ContactComponent,
+    AchievementsComponent,
+    GalleryComponent,
+    FeedbackComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule
   ],
-  providers: [],
+  providers: [FetchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
